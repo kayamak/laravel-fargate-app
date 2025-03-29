463f4dde53f4e2b2c2fa5ee79377ca1c203f35e7
k.kayama <keiichi.kayama@rights-s.jp>
2025-03-29 13:50:43 +0900

    3.8

--- envs/app/foobar/backend.tf
+++ envs/app/foobar/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sample-laravel-fargate-app-tfstate"
-    key = "example/prod/foobar_v1.0.0.tfstate"
+    key    = "example/prod/foobar_v1.0.0.tfstate"
     region = "ap-northeast-1"
   }
 }
