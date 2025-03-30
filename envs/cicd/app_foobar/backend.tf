c8e6e1607639f7d410252acfe46155b8e23a51ec
k.kayama <keiichi.kayama@rights-s.jp>
2025-03-30 13:33:49 +0900

    fix

--- envs/cicd/app_foobar/backend.tf
+++ envs/cicd/app_foobar/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "sample-laravel-fargate-app-tfstate"
-    key            = "example/prod/cicd/app_foobar_v1.0.0.tfstate"
-    region         = "ap-northeast-1"
+    bucket = "sample-laravel-fargate-app-tfstate"
+    key    = "example/prod/cicd/app_foobar_v1.0.0.tfstate"
+    region = "ap-northeast-1"
   }
 }
--- envs/cicd/app_foobar/iam.tf
+++ envs/cicd/app_foobar/iam.tf
@@ -9,16 +9,16 @@ resource "aws_iam_role" "developer" {
   name = "${local.name_prefix}-${local.service_name}-developer"
   assume_role_policy = jsonencode(
     {
-      "Version": "2012-10-17",
-      "Statement": [
+      "Version" : "2012-10-17",
+      "Statement" : [
         {
-          "Effect": "Allow",
-          "Action": [
+          "Effect" : "Allow",
+          "Action" : [
             "sts:AssumeRole",
             "sts:TagSession"
-          ]
-          "Principal": {
-            "AWS": "${aws_iam_user.github.arn}"
+          ],
+          "Principal" : {
+            "AWS" : aws_iam_user.github.arn
           },
         }
       ]
@@ -37,3 +37,4 @@ resource "aws_iam_role_policy_attachment" "role_developer_policy_ecr_power_user"
   role       = aws_iam_role.developer.name
   policy_arn = data.aws_iam_policy.ecr_power_user.arn
 }
+
