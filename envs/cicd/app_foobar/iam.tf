4fb3737de436b666b426a8e3a1e9370077ea42cd
k.kayama <keiichi.kayama@rights-s.jp>
2025-03-30 11:35:46 +0900

    4.9.1

--- envs/cicd/app_foobar/iam.tf
+++ envs/cicd/app_foobar/iam.tf
@@ -4,3 +4,27 @@ resource "aws_iam_user" "github" {
     Name = "${local.name_prefix}-${local.service_name}-github"
   }
 }
+
+resource "aws_iam_role" "developer" {
+  name = "${local.name_prefix}-${local.service_name}-developer"
+  assume_role_policy = jsondecode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "sts:AssumeRole",
+            "sts:TagSession"
+          ]
+          "Principal": {
+            "AWS": "aws_iam_user.github.arn"
+          },
+        }
+      ]
+    }
+  )
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-developer"
+  }
+}
