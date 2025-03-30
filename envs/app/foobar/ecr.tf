0bad67a8238296adb4e8e02b83680b69edc8b49b
k.kayama <keiichi.kayama@rights-s.jp>
2025-03-30 09:11:02 +0900

    3.21.3

--- envs/app/foobar/ecr.tf
+++ envs/app/foobar/ecr.tf
@@ -1,32 +1,4 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "example-prod-foobar-nginx"
-}
-
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 10
-          },
-          "action" : {
-            "type" : "expire"
-          }
-        }
-      ]
-    }
-  )
-
-  repository = aws_ecr_repository.nginx.name
-}
-
 module "nginx" {
   source = "../../../modules/ecr"
-  name = "example-prod-foobar-nginx"
+  name   = "example-prod-foobar-nginx"
 }
-
