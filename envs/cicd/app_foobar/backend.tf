d0e69615f5af0cc919e2a77c90466674bcedd4d2
k.kayama <keiichi.kayama@rights-s.jp>
2025-03-30 11:07:01 +0900

    fix

--- envs/cicd/app_foobar/backend.tf
+++ envs/cicd/app_foobar/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "sample-laravel-fargate-app-tfstate "
+    bucket         = "sample-laravel-fargate-app-tfstate"
     key            = "example/prod/cicd/app_foobar_v1.0.0.tfstate"
     region         = "ap-northeast-1"
   }
